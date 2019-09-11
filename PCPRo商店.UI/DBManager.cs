fe0c89b81b2539d0666be767a86799ff937226de
oyatsu9 <k.kunimoto99@gmail.com>
2019-09-11 23:24:43 +0900

    DBManagerにメソッド（GetTable）を追加

--- PCPRo商店.UI/DBManager.cs
+++ PCPRo商店.UI/DBManager.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data;
+using System.Data.SqlClient;
 
 namespace PCPRo商店.UI
 {
-    class DBManager
+    public class DBManager
     {
+        private const string _connectionString = "※　接続文字列";
+
+        public DataTable GetTable(string strSql, params SqlParameter[]  parameters)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+
+                    foreach (var parameter in parameters)
+                    {
+                        command.Parameters.Add(parameter);
+                    }
+
+                    command.CommandText = strSql;
+
+                    var table = new DataTable();
+
+                    using (var adapter = new SqlDataAdapter(command))
+                    {
+                        adapter.Fill(table);
+
+                        return table;
+                    }
+                }
+
+            }
+        }
     }
 }
